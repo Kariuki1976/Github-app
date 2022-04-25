1c1a90f21c2d982195cffb2bc203461380a2dc8e
Kariuki1976 <emmanuel.mwangi@student.moringaschool.com>
2022-04-26 01:29:53 +0300

    add search image

---
+++ src/app/github-search.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { Repository } from '../repository';
+import { User } from '../user';
+import { HttpClient } from '@angular/common/http';
+@Injectable({
+  providedIn: 'root',
+})
+export class GithubSearchService {
+
+  // constructor() { }
+}
